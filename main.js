d0bb66f0152a77109e6dfec179c427c026bb3f6a
Madison Barnett <Madison@Madisons-MacBook-Pro.local>
2018-04-23 17:39:47 -0400

    just made it prettier

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
 var radius = 10;
+
 // Making the canvas span from top to bottom of screen will change later
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var putPoint = function(e) {
+var putPoint = function (e) {
     // context.arc method will create a circle. It's about to get mathmatical(geometry homies)
     // context.arc uses x, y, radius, start and end angles, and anticlockwise(optional).
     // first parameter is for finding the center of the arc(x, y)
@@ -16,14 +18,16 @@ var putPoint = function(e) {
     // 2PIr is the equation for finding circumference of the cirle which equals 360 degrees. PI radians
     // is equal to 180, PI/2 radians equals 90 degrees and so on. 
     // I'll be leaving out the anticlockwise parameter because I want the default clockwise setting.
-    
+
     context.beginPath();
-   
+
     // e.offset gives us the x and y coordinate of event. Radius is preset to 10 in the variable above, start angle 
     // is 0, and in order to go all the way around the circle we need to use 2PI which is 'Math.PI*2' in JS.
-    context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI*2);
     
+    context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI * 2);
+
     // context.fill will fill the circle.
+    
     context.fill();
 }
 canvas.addEventListener('mousedown', putPoint);
